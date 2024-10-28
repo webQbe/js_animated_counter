77afa746157f03bc54afcd65d7c7fa4e5731f65c
webQbe <webcubesolutionslk@gmail.com>
2024-10-28 13:58:28 +0530

    Counted Increment to reach each target

--- script.js
+++ script.js
@@ -11,8 +11,15 @@ counters.forEach(counter => {
 
         // Get Current Data Target Value
         const target = +counter.getAttribute('data-target');
+
+        // Get Current Element's Display Value as A Number
+        const count = +counter.innerText;
+
+        // Get Step Count to Reach Target
+        const step = target / speed;
+
         console.log(target);
-        console.log(typeof target);
+        console.log(step);
 
     }
 
