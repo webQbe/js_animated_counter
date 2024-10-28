5e29d822d5154d73ccab5028c7f706d854ed30a7
webQbe <webcubesolutionslk@gmail.com>
2024-10-28 13:46:18 +0530

    Logged target value of each counter

---
+++ script.js
@@ -0,0 +1,20 @@
+// Define DOM Elements
+const counters = document.querySelectorAll('.counter');
+
+// Set Count Speed
+const speed = 200;
+
+// Loop through Counters Array
+counters.forEach(counter => {
+
+    const updateCount = () => {
+
+        // Get Current Data Target Value
+        const target = counter.getAttribute('data-target');
+        console.log(target);
+
+    }
+
+    updateCount();
+
+});
