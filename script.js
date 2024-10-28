8d2b0710b05992a23f8746d7d4504027774c3ceb
webQbe <webcubesolutionslk@gmail.com>
2024-10-28 14:16:51 +0530

    Calculated & displayed counter values until targets are reached

--- script.js
+++ script.js
@@ -12,14 +12,35 @@ counters.forEach(counter => {
         // Get Current Data Target Value
         const target = +counter.getAttribute('data-target');
 
-        // Get Current Element's Display Value as A Number
+        // Get Current Element Value as A Number
         const count = +counter.innerText;
 
-        // Get Step Count to Reach Target
+        // Get Increment Count to Reach Target
         const step = target / speed;
 
         console.log(target);
         console.log(step);
+
+        // Check if We've reached Target 
+        if(count < target){
+
+            // When we've not reached Target yet
+            // Calculate Display Value by 
+            // Adding Increment to Current Display Value
+            counter.innerText = count + step;
+
+            // Run updateCount() every 1ms
+            setTimeout(updateCount, 1);
+
+
+        } else {
+
+            // When we've reached Target 
+            // Display Target Value
+            count.innerText = target;
+
+        }
+
 
     }
 
